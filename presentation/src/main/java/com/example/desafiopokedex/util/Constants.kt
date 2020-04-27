207a03b871f709e93b6d24155c4e1dc88b876b01
Paulo Renato <paulogalindo94@hotmail.com>
2020-04-26 22:06:21 -0400

    Created:  - Service layer to create requests.  - File with constants.  - Base to activities, fragments  - Factory to create instances of a ViewModel.

---
+++ presentation/src/main/java/com/example/desafiopokedex/util/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.desafiopokedex.util
+
+const val API_ENDPOINT = "API_ENDPOINT"
---
+++ presentation/src/main/java/com/example/desafiopokedex/view/base/BaseViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.desafiopokedex.view.base
+
+import androidx.lifecycle.ViewModel
+
+class BaseViewModel : ViewModel() {
+
+}
