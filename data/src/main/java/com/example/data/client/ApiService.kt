cda19a62c935ff24d396828f8fbdf8bf8bdf297d
Paulo Renato <paulogalindo94@hotmail.com>
2020-04-27 20:50:45 -0400

    Altered:  - Package data included request list of pokemons

--- data/src/main/java/com/example/data/client/ApiService.kt
+++ data/src/main/java/com/example/data/client/ApiService.kt
@@ -1,4 +1,15 @@
 package com.example.data.client
 
+import com.example.data.entities.ApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("pokemon/")
+    suspend fun getListPokemon(
+        @Query("offset") offset: Int,
+        @Query("limit") limit: Int
+    ): Response<ApiResponse>
 }
---
+++ data/src/main/java/com/example/repository/GetListResultRepositoryImpl.kt
@@ -0,0 +1,17 @@
+package com.example.repository
+
+import com.example.data.client.ApiClient
+import com.example.data.entities.ApiResponse
+import com.example.data.util.Mapper
+import com.example.domain.entity.Response
+import com.example.domain.repository.GetListResultRepository
+import javax.inject.Inject
+
+class GetListResultRepositoryImpl @Inject constructor(
+    private val apiClient: ApiClient,
+    private val mapper: Mapper<ApiResponse, Response>
+) : GetListResultRepository {
+    override suspend fun getListPokemon(offset: Int, limit: Int): Response? {
+        return apiClient.getListPokemon(offset, limit)?.let(mapper::transform)
+    }
+}
