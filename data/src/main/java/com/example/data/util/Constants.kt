906a9b1bc740c3f512c4efddf1a28177caf9de9d
Paulo Renato <paulogalindo94@hotmail.com>
2020-05-01 22:45:00 -0400

    Altered:  - RequestHandle to handling errors  - Constants with messages errors

---
+++ data/src/main/java/com/example/data/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.data.util
+
+const val UNKNOWN_ERROR = "Um erro inesperado aconteceu. Por favor, tente mais tarde!"
+const val SOCKET_ERROR = "Servidor demorou obter uma resposta. Por favor, tente mais tarde!"
+const val HTTP_ERROR = "Não foi possível completar sua ação. Por favor, tente novamente!"
+const val NETWORK_ERROR = "Não foi possível completar sua ação. Por favor, verifique sua conexão!"
--- data/src/main/java/com/example/data/util/RequestHandler.kt
+++ data/src/main/java/com/example/data/util/RequestHandler.kt
@@ -1,7 +1,13 @@
 package com.example.data.util
 
+import com.example.data.entities.ApiError
+import com.google.gson.Gson
 import kotlinx.coroutines.coroutineScope
+import okhttp3.ResponseBody
+import retrofit2.HttpException
 import retrofit2.Response
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 open class RequestHandler {
 
@@ -12,12 +18,33 @@ open class RequestHandler {
                     if (isSuccessful) {
                         body()
                     } else {
-                        throw Throwable()
+                        throw Exception(responseError(errorBody()))
                     }
                 }
             } catch (e: Exception) {
-                throw e
+                throw Exception(
+                    when (e) {
+                        is HttpException -> HTTP_ERROR
+                        is UnknownHostException -> NETWORK_ERROR
+                        is SocketTimeoutException -> SOCKET_ERROR
+                        else -> UNKNOWN_ERROR
+                    }
+                )
             }
+        }
+    }
+
+    private fun responseError(responseError: ResponseBody?): String? {
+        return extractError(
+            Gson().fromJson(responseError?.string(), ApiError::class.java)
+        )
+    }
+
+    private fun extractError(apiError: ApiError?): String? {
+        return if (!apiError?.errors.isNullOrEmpty()) {
+            apiError?.errors?.joinToString("\n")
+        } else {
+            apiError?.message
         }
     }
 }
